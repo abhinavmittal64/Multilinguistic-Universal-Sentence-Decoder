bbdd48617a31e645b3c048c8b48f675c90eb03f4
Abhinav A Mittal <abhinav.a.mittal@oracle.com>
2019-08-03 16:47:02 +0530

    pythonScript Added

---
+++ setup.sh
@@ -0,0 +1,7 @@
+!pip uninstall --quiet --yes tensorflow
+!pip install --quiet tensorflow-gpu==1.13.1
+!pip install --quiet tensorflow-hub
+!pip install --quiet bokeh
+!pip install --quiet tf-sentencepiece
+!pip install --quiet simpleneighbors
+!pip install --quiet tqdm
